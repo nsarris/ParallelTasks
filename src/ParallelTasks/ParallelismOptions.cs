cd32ef59f2211dfe417558c029e89baf7f8a8d59
Νίκος Σαρρής <nsarris@vivawallet.com>
2021-04-18 20:04:19 +0300

    Added DefaultDegreeOfParallelism.

---
+++ src/ParallelTasks/ParallelismOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace ParallelTasks
+{
+    /// <summary>
+    /// Ddefault parallelism options.
+    /// </summary>
+    public static class ParallelismOptions
+    {
+        /// <summary>
+        /// The default degree of parallelism in enumerable sourced parallel operations. Set to 0 to disable. Default is 0.
+        /// </summary>
+        public static int DefaultDegreeOfParallelism { get; } = 0;
+    }
+}
